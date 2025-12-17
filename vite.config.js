3974845415d70935822e9cff0441d3b57a507736
shubhamyadav-29 <shubhamarjun5@gmail.com>
2025-12-17 11:25:01 +0530

    Deploy build using docs folder for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/My_portfolio/",
+  build: {
+    outDir: "docs",
+    emptyOutDir: true,
+  },
 });
 
 // import { defineConfig } from 'vite'
